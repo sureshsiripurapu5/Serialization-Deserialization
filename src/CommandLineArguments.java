7a4a4a2df749bf507f73a72f35e2e75bfa774727
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-10-04 20:32:00 +0530

    command-line-arguments

---
+++ src/CommandLineArguments.java
@@ -0,0 +1,9 @@
+public class CommandLineArguments {
+    public static void main(String[] suresh) {
+        int a = Integer.parseInt(suresh[0]);
+        int b= Integer.parseInt(suresh[1]);
+        int c= a+b;
+        System.out.println(c);
+
+    }
+}
